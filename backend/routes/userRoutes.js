be0065971e137358faab7968faaa33cb668a060a
Yelim Lee <100793454+dPfal@users.noreply.github.com>
2026-05-11 15:33:07 +1000

    Revert "Dev"

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,13 +4,11 @@ const router = express.Router();
 const {
   getListName,
   updateListName,
-  updateProfile,
 } = require("../controllers/userController");
 
 const { protect } = require("../middleware/authMiddleware");
 
 router.get("/list-name", protect, getListName);
 router.put("/list-name", protect, updateListName);
-router.put("/profile", protect, updateProfile);
 
 module.exports = router;
