57a434b52b0311eb684eb40f0f415d348e7ec109
Jayden-cuu <jayden.cuu@gmail.com>
2026-05-11 13:44:18 +1000

    implement singleton pattern for database connextion.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
 const listRoutes = require("./routes/groceryListRoutes");
@@ -23,16 +23,10 @@ app.use("/api/lists", listRoutes);
 
 const PORT = process.env.PORT || 5001;
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch(error => {
-    console.error("MongoDB connection error:", error);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
+});
 
 module.exports = app;
